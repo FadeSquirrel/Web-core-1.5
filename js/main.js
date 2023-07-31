19e326b3fea87c407146cf605b170090f47f52c4
FadeSquirrel <superbelochko@gmail.com>
2023-07-31 22:27:43 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const swiper = new Swiper('.swiper', {
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+			enabled: true,
+		},
+		768: {
+			enabled: false,
+		}
+	},
+
+	pagination: {
+		el: '.swiper-pagination',
+	},
+});
+
+let showMore = document.querySelector('.show-more');
+
+showMore.addEventListener('click', () => {
+	let hiddenButton = document.querySelectorAll('[data-hidden="true"]');
+	let hiddenPcButton = document.querySelectorAll('[data-hidden-pc="true"]');
+
+	hiddenButton.forEach(element => {
+		element.classList.toggle('hidden');
+	});
+	
+	hiddenPcButton.forEach(element => {
+		element.classList.toggle('hidden-pc');
+	});
+
+	if (showMore.classList.contains("show-more--decoration-rotate")) {
+		showMore.textContent = "Показать все";
+		showMore.classList.remove("show-more--decoration-rotate");
+
+	} else {
+		showMore.textContent = "Скрыть";
+		showMore.classList.add("show-more--decoration-rotate");
+	}
+
+})
